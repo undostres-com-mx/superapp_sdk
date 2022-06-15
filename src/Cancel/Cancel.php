09635e3975e88f7d73d9511a95444021f4fdcbb4
CarlosAMS123 <carlos.miranda@undostres.com.mx>
2022-06-14 21:09:59 -0500

    Fix Cancel Constructor order
    Bug where payment was not setted because order execution

--- src/Cancel/Cancel.php
+++ src/Cancel/Cancel.php
@@ -23,11 +23,11 @@ class Cancel
    */
   public function __construct($host, $appKey, $appToken, $payload)
   {
-    $this->cancelEndpoint = $this->createCancelUrl($host, $this->paymentId);
     $this->appKey         = $appKey;
     $this->appToken       = $appToken;
     $this->paymentId      = $payload["paymentId"];
     $this->requestId      = $payload["paymentId"] . date("YmdHisu");
+    $this->cancelEndpoint = $this->createCancelUrl($host, $this->paymentId);
     $this->payloadJSON = Utils::encodePayload([
       "paymentId"     => $this->paymentId,
       "requestId"     => $this->requestId
@@ -54,12 +54,7 @@ class Cancel
    */
   public function requestCancel()
   {
-    if (!isset($this->payloadJSON))
-      throw new \Exception("Payload not set",503);
-
-    if (strpos($this->cancelEndpoint, "{paymentId}") !== false)
-      throw new \Exception("PaymentId not set in URL", 503);
-
+    if (!isset($this->payloadJSON)) throw new \Exception("Payload not set", 503);
     $response = Utils::request($this->cancelEndpoint, $this->payloadJSON, $this->appKey, $this->appToken);
     Utils::validateData($response, "SuperappCancelPaymentResponse.json");
 
