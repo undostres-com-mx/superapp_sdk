75baf3e77e7a02d1048262654d336fa53b57c6f8
CarlosAMS123 <carlos.miranda@undostres.com.mx>
2022-07-01 11:01:15 -0500

    Update SDK

--- src/SDK/SDK.php
+++ src/SDK/SDK.php
@@ -9,61 +9,95 @@ use UDT\Utils;
 
 class SDK
 {
-
-  private $appKey;
-  private $appToken;
-  private $urlCancel;
-  private $urlRefund;
-  private $urlPayment;
+  private $is_set = false;
+  private $api_key;
+  private $api_token;
+  private $app_key;
+  private $app_token;
+  private $url_cancel;
+  private $url_refund;
+  private $url_payment;
   private $host;
+  private $mode;
+  private $hash = '72253f579e7dc003da754dad4bd403a6';
 
   /**
-   * Constructs a new SDK object able to communicate with the UnDosTres' API.
-   *
-   * @param array $config   Dictionary with condifg data
+   * RECEIVE ENCRYPTED DATA AND CONSTRUCT API TO COMUNICATE WITH UDT
    */
-  public function __construct(array $config)
+  public function __construct(string $keyConfig, string $mode)
   {
-    $this->appKey      = $config['appKey'];
-    $this->appToken    = $config['appToken'];
-    $this->host        = $config['host'];
-    $this->urlCancel   = $this->host . $config['urlCancel'];
-    $this->urlRefund   = $this->host . $config['urlRefund'];
-    $this->urlPayment  = $this->host . $config['urlPayment'];
+    $config = $this->decrypt($keyConfig);
+    if ($config != false) {
+      if ($mode === 'production') $this->host = 'https://undostres.com.mx';
+      else if ($mode === 'testing') $this->host = 'https://test.undostres.com.mx';
+      else if ($mode === 'nobugs') $this->host = 'https://nobugs.undostres.com.mx';
+      else $this->host = 'http://localhost:8081';
+      $config = json_decode($config);
+      $this->api_key      = $config->api_key;
+      $this->api_token    = $config->api_token;
+      $this->app_key      = $config->app_key;
+      $this->app_token    = $config->app_token;
+      $this->url_cancel   = $this->host . $config->url_cancel;
+      $this->url_refund   = $this->host . $config->url_refund;
+      $this->url_payment  = $this->host . $config->url_payment;
+      $this->is_set       = true;
+      $this->mode         = $mode;
+    }
   }
 
   /**
-   * Receive POST data and create a new order for UnDosTres' API.
+   * ENCRYPT USING 3DES WITH MD5
+   * 
+   * @return string
+   */
+  public function encrypt($data)
+  {
+    return base64_encode(openssl_encrypt($data, 'DES-EDE3', $this->hash, OPENSSL_RAW_DATA));
+  }
+
+  /**
+   * DECRYPT USING 3DES WITH MD5
+   * 
+   * @return string
+   */
+  public function decrypt($data)
+  {
+    return openssl_decrypt(base64_decode($data), 'DES-EDE3', $this->hash, OPENSSL_RAW_DATA);
+  }
+
+  /**
+   * VERIFICATION OF REQUEST HEADERS 
+   * 
+   * @return bool
+   */
+  public function is_authentic_request($api_key, $api_token)
+  {
+    return $this->api_key === $api_key && $this->api_token === $api_token;
+  }
+
+  /**
+   * HANDLE PAYLOAD API - VALIDATE, COMUNICATE WITH UDT, VALIDATE, RETURN
    *
-   * @return array
+   * @return string
    */
   public function handlePayload($requestJSON)
   {
-    $response = [
-      "code" => 400,
-      "body" => ["status" => "error"]
-    ];
-
-    $body = json_decode($requestJSON, true);
-
     try {
-      if (isset($body["payment"])) {
+      $body = json_decode($requestJSON, true);
+      if (isset($body['payment'])) {
         Utils::validateData($body["payment"], "SuperappCreatePaymentRequest.json");
-        $response = $this->createPayment($body["payment"]);
-      } else if (isset($body["cancel"])) {
+        $response = $this->createPayment($body['payment']);
+      } else if (isset($body['cancel'])) {
         Utils::validateData($body["cancel"], "SuperappCancelPaymentRequest.json");
-        $response = $this->createCancel($body["cancel"]);
-      } else if (isset($body["refund"])) {
+        $response = $this->createCancel($body['cancel']);
+      } else if (isset($body['refund'])) {
         Utils::validateData($body["refund"], "SuperappRefundPaymentRequest.json");
-        $response = $this->createRefund($body["refund"]);
-      } else throw new \Exception("Undefined method to handle.", 501);
+        $response = $this->createRefund($body['refund']);
+      } else throw new \Exception("No existe el metodo.", 500);
     } catch (\Exception $e) {
       $response = [
-        "code" => $e->getCode(),
-        "body" => [
-          "status" => "error",
-          "error" => $e->getMessage()
-        ]
+        'code' => $e->getCode(),
+        'status' => $e->getMessage(),
       ];
     } finally {
       return $response;
@@ -78,9 +112,9 @@ class SDK
   private function createPayment($paymentData)
   {
     $payment = new Payment(
-      $this->urlPayment,
-      $this->appKey,
-      $this->appToken,
+      $this->url_payment,
+      $this->app_key,
+      $this->app_token,
       $paymentData
     );
 
@@ -88,14 +122,11 @@ class SDK
 
     $queryParams = [];
     parse_str(parse_url($response->paymentUrl)['query'], $queryParams);
-
-    return [
-      "code" => 200,
-      "body" => [
-        "status" => "success",
-        "queryParams" => $queryParams
-      ]
-    ];
+    $url = $queryParams['url'];
+    if ($this->mode == 'localhost')
+      $url = str_replace('https://test.undostres.com.mx', 'http://localhost:8081', $url);
+    $url = 'undostres://home?stage=' . $queryParams['stage'] . '&url=' . urlencode($url);
+    return ['code' => 200, 'status' => 'Success', 'response' => $url];
   }
 
   /**
@@ -106,20 +137,15 @@ class SDK
   private function createCancel($cancelData)
   {
     $cancel = new Cancel(
-      $this->urlCancel,
-      $this->appKey,
-      $this->appToken,
+      $this->url_cancel,
+      $this->app_key,
+      $this->app_token,
       $cancelData
     );
 
     $cancel->requestCancel();
 
-    return [
-      "code" => 200,
-      "body" => [
-        "status" => "success"
-      ]
-    ];
+    return ['code' => 200, 'status' => 'Success'];
   }
 
   /**
@@ -130,21 +156,16 @@ class SDK
   private function createRefund($refundData)
   {
     $refund = new Refund(
-      $this->urlRefund,
-      $this->appKey,
-      $this->appToken,
+      $this->url_refund,
+      $this->app_key,
+      $this->app_token,
       $refundData["paymentId"],
       $refundData["transactionId"],
-      round(floatval($refundData["value"]), 2)
+      $refundData["value"]
     );
 
     $refund->requestRefund();
 
-    return [
-      "code" => 200,
-      "body" => [
-        "status" => "success"
-      ]
-    ];
+    return ['code' => 200, 'status' => 'Success'];
   }
 }
